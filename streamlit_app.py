4c1c47376cf774d583ad8a1ee292f05d5f5ce8b6
PatrickTCHOMTE <121275250+PatrickTCHOMTE@users.noreply.github.com>
2023-05-29 16:18:51 +0200

    Add files via upload

---
+++ streamlit_app.py
@@ -0,0 +1,12 @@
+import streamlit as st
+from st_files_connection import FilesConnection
+
+# Create connection object and retrieve file contents.
+# Specify input format is a csv and to cache the result for 600 seconds.
+conn = st.experimental_connection('s3', type=FilesConnection)
+df = conn.read("nutri-ecolo-app-data/open_food_data_final_cleaned.csv", input_format="csv")
+
+# Display a dataframe as an interactive table
+st.dataframe(df, None, 200)
+
+# Print results.
